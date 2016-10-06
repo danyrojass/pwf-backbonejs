6bd62cd6536d84c9e85bc13b87938109a15cef3b
danyrjoass <dany@dany-HP>
2016-10-06 18:31:45 -0300

    Segundo commit: Agregamos el método POST
    Realizamos el módulo que nos permite crear un nuevo contacto.

--- app/js/view/formulario-persona-view.js
+++ app/js/view/formulario-persona-view.js
@@ -1,8 +1,9 @@
 /**
  * Clase que implementa el Formulario de alta de personas.
  * @class
- */
+ */var cont=0;
 var FormularioPersonaView = Backbone.View.extend({
+
     /**
      * Url del template que corresponde al view
      * @field
@@ -44,11 +45,58 @@ var FormularioPersonaView = Backbone.View.extend({
      */
     guardar: function () {
         var data = {};
+        var flag = 0;
+        var flagmail = 0;
         //por cada input del view
         this.$el.find("[name]").each(function () {
+          if (this.name == "nombre" && this.value == ""){
+            flag = 1;
+            alert("Nombre, campo obligatorio");
+            return 0;
+          }
+          if (this.name == "apellido" && this.value == ""){
+            flag = 1;
+            alert("Apellido, campo obligatorio");
+            return 0;
+          }
+          expr = /^[0-9]+$/;
+          if (this.name == "telefono")
+          if (!expr.test(this.value)){
+            flag = 1;
+            alert("Telefono, campo obligatorio y Numerico");
+            return 0;
+          }
+
+            if (this.name == "email"){
+              flagmail = validarEmail(this.value);
+              //console.log(flagmail);
+              //console.log(this.value);
+              if (flagmail==0 && this.name == "email") data[this.name] = this.value;
+              return 0;
+            }
+
+
             data[this.name] = this.value;
+
+            var today = new Date();
+            data["fechacreacion"] = today.toISOString().substring(0, 10);
+
+
         });
-        var model = new PersonaModel(data);
-        this.collection.add(model);
-    }
+        if (flag == 0 && flagmail == 0){
+          var model = new PersonaModel(data);
+
+          model.save({}, {
+              success: function (model, respose, options) {
+                  window.alert("¡Contacto creado!");
+                  window.location.reload();
+              },
+              error: function (model, xhr, options) {
+                  window.alert("Imposible crear el contacto.");
+                  window.location.reload();
+              }
+          });
+        }
+    },
+
 });
